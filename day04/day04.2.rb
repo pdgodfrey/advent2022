0de0f0a62f9eceade836dacf67d6a357ce66987e
Paul Godfrey <paul@metarhythm.com>
2022-12-04 00:49:29 -0500

    Day 4

---
+++ day04/day04.2.rb
@@ -0,0 +1,28 @@
+
+file = File.open('./input.txt', 'r')
+
+data = file.read
+
+pairs = data.split(/\n/)
+
+score = 0
+
+pairs.each{|pair|
+  first, second = pair.split(",")
+
+  first_min, first_max = first.split("-")
+  second_min, second_max = second.split("-")
+
+  first_min = first_min.to_i
+  first_max = first_max.to_i
+  second_min = second_min.to_i
+  second_max = second_max.to_i
+
+  if first_min.between?(second_min, second_max) || first_max.between?(second_min, second_max) ||
+     second_min.between?(first_min, first_max) || second_max.between?(first_min, first_max)
+    score +=1
+  end
+
+}
+
+puts score
